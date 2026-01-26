194289805a6ee91c14bd6872393636432484d8b5
oscarsidsa81 <ovilches@sid-sa.com>
2026-01-26 11:16:55 +0100

    Primer commit

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    "name": "SIDSA - Purchase Views",
+    "name": "SIDSA - Purchase Views OV",
     "version": "15.0.1.0.0",
     "category": "Purchases",
     "summary": "Overrides de vistas de Purchase agrupados por funcionalidad.",
@@ -13,17 +13,14 @@
     ],
     "data": [
         "views/purchase_order/tree_base.xml",
-        "views/purchase_order/form_buttons.xml",
         "views/purchase_order/form_varios.xml",
-        "views/purchase_order_line/tree_buttons.xml",
         "views/purchase_order_line/tree_columns.xml",
         "views/purchase_order_line/tree_decorations.xml",
-        "views/purchase_order_line/form_contract_date.xml",
         "views/purchase_order_line/search.xml",
     ],
     "assets": {
         "web.assets_backend": [
-            "sid_purchase_views/static/src/scss/sid_list_wrap.scss",
+            "sid_purchase_views_ov/static/src/scss/sid_list_wrap.scss",
         ],
     },
     "installable": True,
